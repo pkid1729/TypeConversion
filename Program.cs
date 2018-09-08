640c8fd6034dc3e4e0c1e0880dbadc5e10447819
pkid1729 <30475130+pkid1729@users.noreply.github.com>
2018-09-08 01:39:59 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = "123456";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+           
+            try
+            {
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("The numer could not be converted into byte");
+            }
+            try
+            {
+                string str = "true";
+                bool bo = Convert.ToBoolean(str);
+                Console.WriteLine(bo);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
